dcf24fcecfbade39a62ab0fdfed50b27d6e15a3a
taushik2106-ios <taushik2106@gmail.com>
2026-01-07 00:17:53 +0530

    Add initial ads data for campaigns

---
+++ data.py
@@ -0,0 +1,5 @@
+ads_data = [
+    {"ad_id": 1, "campaign": "Tech Launch", "impressions": 10000, "clicks": 420},
+    {"ad_id": 2, "campaign": "Sports Promo", "impressions": 8500, "clicks": 310},
+    {"ad_id": 3, "campaign": "Streaming Ad", "impressions": 12000, "clicks": 670}
+]
